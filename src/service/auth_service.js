20d4afc2fbbd8adc665f60d33eea546994660b82
Taewook <rhkrxodnr@gmail.com>
2022-01-06 11:19:04 +0900

    add firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase';
+
+class AuthService {
+    login(providerName) {
+        const authProvider = new firebase.auth.[`${providerName}AuthProvider`]();
+        return firebase.auth().signInWithPopup(authProvider);
+    }
+}
+export default AuthService
---
+++ src/service/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API - KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
